2af688a43e83793e5d4e63267b91b6feafb151b8
akari16@163.com <akari16@163.com>
2020-12-07 23:54:03 +0800

    删除部分代码
    删除部分代码

--- debounceThrottle/debounce.js
+++ debounceThrottle/debounce.js
@@ -1,18 +1,3 @@
-/*
- * fn [function] 需要防抖的函数
- * delay [number] 毫秒，防抖期限值
- */
-/* function debounce(fn,delay){
-    let timer = null //借助闭包
-    return function() {
-        if(timer){
-            clearTimeout(timer) //进入该分支语句，说明当前正在一个计时过程中，并且又触发了相同事件。所以要取消当前的计时，重新开始计时
-            timer = setTimeout(fn,delay) 
-        }else{
-            timer = setTimeout(fn,delay) // 进入该分支说明当前并没有在计时，那么就开始一个计时
-        }
-    }
-} */
 
 /*
   对于短时间内连续触发的事件（上面的滚动事件,包含点击事件，用户不断点击等），
