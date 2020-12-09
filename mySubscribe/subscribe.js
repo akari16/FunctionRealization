d973658bac493266c7972f845e3946b0c77b2061
akari16@163.com <akari16@163.com>
2020-12-09 23:31:12 +0800

    Modify code format
    Modify code format

--- mySubscribe/subscribe.js
+++ mySubscribe/subscribe.js
@@ -1,90 +1,39 @@
-/* var Event = function () {
-  this.list = [];
-};
-
-Event.prototype.add = function (listener) {
-  this.list.push(listener);
-};
-
-Event.prototype.triggle = function () {
-  this.list.forEach((listener) => {
-    console.log(listener);
-    listener();
-  });
-}; */
-
-// 没有取消订阅的功能
-/* var event = {
-  list: {},
-  add(type, listener) {
-    if (!this.list[type]) {
-      this.list[type] = [];
-    }
-    this.list[type].push(listener);
-  },
-  triggle(type) {
-    this.list[type] &&
-      this.list[type].forEach((listener) => {
-        listener();
-      });
-  },
-}; */
-
-/* // 添加订阅功能
-var event = {
-  list: {},
-  add(type, listener) {
-    if (!this.list[type]) {
-      this.list[type] = [];
-    }
-    this.list[type].push(listener);
-  },
-  triggle(type) {
-    this.list[type] &&
-      this.list[type].forEach((listener) => {
-        listener();
-      });
-  },
-  remove(type, fn) {
-    if (!this.list[type]) return;
-    var index = this.list[type].findIndex((listener) => listener === fn);
-    this.list[type].splice(index, 1);
-  },
-};
- */
-
-//  添加历史记录功能
-var Event = function () {
+/*
+  初始化代码添加历史记录功能，
+  添加信息订阅功能
+*/ 
+var Messege = function () {
   this.list = {};
   this.cache = {};
 };
 
-Event.prototype.add = function (area, client) {
-  // 房屋面积加载到订阅列表
-  console.log(area);
+Messege.prototype.add = function (noticeType, client) {
+  // 将收到的信息加入到noticeType订阅列表当中
+  console.log(noticeType);
   console.log(client);
 
-  if (!this.list[area]) this.list[area] = [];
-  this.list[area].push(client);
-  this.cache[area].forEach((price) => {
-    client.listen(area, price);
+  if (!this.list[noticeType]) this.list[noticeType] = [];
+  this.list[noticeType].push(client);
+  this.cache[noticeType].forEach((words) => {
+    client.listen(noticeType, words);
   });
 };
 
-Event.prototype.remove = function (area, client) {
-  if (!this.list[area]) return;
-  var index = this.list[area].findIndex((item) => item === client);
-  console.log(this.list[area].splice(index, 1));
-  this.list[area].splice(index, 1);
+// 通过传入的信息类型，遍历查找
+Messege.prototype.remove = function (noticeType, client) {
+  if (!this.list[noticeType]) return; //可以作为提示或者说处理符合业务需求的操作
+  var index = this.list[noticeType].findIndex((item) => item === client);
+  console.log(this.list[noticeType].splice(index, 1));
+  this.list[noticeType].splice(index, 1);
 };
 
-Event.prototype.triggle = function (area, price) {
-  if (!this.cache[area]) this.cache[area] = [];
-  this.cache[area].push(price);
+Messege.prototype.triggle = function (noticeType, words) {
+  if (!this.cache[noticeType]) this.cache[noticeType] = [];
+  this.cache[noticeType].push(words);
 
-  if (!this.list[area]) return;
-  this.list[area].forEach((client) => {
-    client.listen(area, price);
+  if (!this.list[noticeType]) return;
+  this.list[noticeType].forEach((client) => {
+    client.listen(noticeType, words);
   });
 };
 
@@ -93,6 +42,6 @@ var Client = function (name) {
 };
 
 // 监听事件，事件处理逻辑
-Client.prototype.listen = function (area, price) {
-  console.log(`${this.name}收到${area}平的房源报价${price}`);
+Client.prototype.listen = function (noticeType, words) {
+  console.log(`${this.name}收到${noticeType}的信息是：${words}`);
 };
